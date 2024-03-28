b9605d606396069e5d8fb3848fa2154325deacfa
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2024-03-28 16:32:16 -0300

    Add files via upload

---
+++ FireConfig.js
@@ -0,0 +1,11 @@
+firebaseConfig = {
+    apiKey: "AIzaSyBcSLa4ltsV72t_47YMva7znqaHHsslL6w",
+    authDomain: "agendamento-hd.firebaseapp.com",
+    databaseURL: "https://agendamento-hd-default-rtdb.firebaseio.com",
+    projectId: "agendamento-hd",
+    storageBucket: "agendamento-hd.appspot.com",
+    messagingSenderId: "558624460897",
+    appId: "1:558624460897:web:8dafb92e36acf0cf789a20"
+  };
+
+  firebase.initializeApp(firebaseConfig);
